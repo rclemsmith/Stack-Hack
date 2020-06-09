def92fb2827ad679045db82a3941b9e7653195cf
Clement Smith <43886157+rclemsmith@users.noreply.github.com>
2020-06-09 12:06:33 +0530

    Update config.js

--- Task/BackEnd/config.js
+++ Task/BackEnd/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': 'rclemsmith',
-    'mongoUrl' : "MongoAtlasUrlHere"
+    'mongoUrl' : "mongodb+srv://rclemsmith:Joker2008@cluster0-hlz82.mongodb.net/test?retryWrites=true&w=majority"
 }
